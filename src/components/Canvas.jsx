d32e859d7a59acc3b9b850152e99f691c2dd6101
KelvinQiu <kelvinqiu802@outlook.com>
2022-10-04 20:29:38 +0800

    feat: drag component from list to canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,20 +1,47 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
 import { useDrop } from 'react-dnd';
 import CustomButton from './custom/CustomButton';
 import Resizable from './custom/Resizable';
 
 function Canvas({ data, setData, selected, setSelected }) {
+  const elem = React.useRef();
+
   const [, drop] = useDrop(() => ({
     accept: 'component',
-    drop(item, monitor) {},
+    drop: (item, monitor) => handleDrop(item, monitor),
   }));
+
+  const handleDrop = (item, monitor) => {
+    if (item.tag === 'button') {
+      setData((prev) => [
+        ...prev,
+        {
+          tag: 'button',
+          id: nanoid(),
+          props: {
+            text: 'Kelvin',
+            variant: 'outlined',
+            style: {
+              position: 'absolute',
+              width: 100,
+              height: 30,
+              left: monitor.getClientOffset().x - elem.current.offsetLeft,
+              top: monitor.getClientOffset().y - elem.current.offsetTop,
+            },
+          },
+        },
+      ]);
+    }
+  };
 
   const select = (id) => {
     setSelected(id);
   };
 
+  drop(elem);
   return (
-    <div className='canvas' ref={drop}>
+    <div className='canvas' ref={elem}>
       {data.map((item) => (
         <Resizable
           key={item.id}
